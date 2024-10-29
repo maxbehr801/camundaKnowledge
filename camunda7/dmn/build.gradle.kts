db420eb195e7c306648d85246d1f296cf07ccb1f
Maximilian Behrendt <maximilian.behrendt@envite.de>
2024-10-29 13:22:37 +0100

    externalize java version from build script and rename CAMUNDA_VERSION property

--- camunda7/dmn/build.gradle.kts
+++ camunda7/dmn/build.gradle.kts
@@ -2,11 +2,11 @@ plugins {
     id("c7-conventions")
 }
 
-val CAMUNDA_VERSION: String by project
+val CAMUNDA_7_VERSION: String by project
 val JUEL_VERSION = "2.2.7"
 
 dependencies {
-    testImplementation("org.camunda.bpm.dmn:camunda-dmn-junit5:$CAMUNDA_VERSION")
+    testImplementation("org.camunda.bpm.dmn:camunda-dmn-junit5:$CAMUNDA_7_VERSION")
     testImplementation("de.odysseus.juel:juel-api:${JUEL_VERSION}")
     testImplementation("de.odysseus.juel:juel-impl:${JUEL_VERSION}")
     testImplementation("de.odysseus.juel:juel-spi:${JUEL_VERSION}")
